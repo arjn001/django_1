f64caacde814b62f429883a719f3923ac4dd0a9b
arjn001 <arjungyawali001@gmail.com>
2022-06-03 07:55:58 +0545

    subcat complete

--- home/views.py
+++ home/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import *
 # Create your views here.
+
+class BaseView(View):
+	views = {}
+	views['categories'] = Category.objects.all()
+	views['subcategories'] = SubCategory.objects.all()
+
+	
+class HomeView(BaseView):
+	def get(self,request):
+		
+		self.views['new_products'] = Product.objects.filter(labels = 'new')
+		self.views['hot_products'] = Product.objects.filter(labels = 'hot')
+		self.views['offer_products'] = Product.objects.filter(labels = 'offer')
+		self.views['sliders'] = Slider.objects.all()
+		self.views['ads'] = Ad.objects.all()
+		return render(request,'index.html', self.views)
+
+
+class SubCategoryView(BaseView):
+	def get(self,request,slug):
+		sub_cat = SubCategory.objects.get(slug = slug).id
+		self.views['subcat_products'] = Product.objects.filter(subcategory_id = sub_cat)
+		return render(request,'subcategory.html', self.views)
+
+
+class DetailView(BaseView):
+	def get(self,request,slug):
+		self.views['detail_products'] = Product.objects.filter(slug = slug)
+		return render(request,'single.html', self.views)
+
